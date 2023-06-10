2e0c33052ccd1e34c2cf1275f04a1be52f66a776
Amiel Luis Labajo <amiellabajo05@gmail.com>
2023-06-11 00:49:01 +0800

    adjusted production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.require_master_key = true
 end
